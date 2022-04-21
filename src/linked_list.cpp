d1ad52d1912363d9e17ca48b414d534d8dee3a58
Ilyas <ilyaskalimullinn@yandex.ru>
2022-04-21 18:35:54 +0300

    finished task

--- src/linked_list.cpp
+++ src/linked_list.cpp
@@ -12,7 +12,18 @@ namespace assignment {
    *    по времени ~ O(N)
    */
   void LinkedList::ReverseIterative() {
-    // Напишите здесь свой код ...
+    if (front_ == nullptr) return;
+    Node* prev = nullptr;
+    Node* curr = front_;
+    front_ = back_;
+    back_ = curr;
+
+    while (curr != nullptr) {
+      Node* next = curr->next;
+      curr->next = prev;
+      prev = curr;
+      curr = next;
+    }
   }
 
   /**
@@ -23,12 +34,19 @@ namespace assignment {
    *    по времени ~ O(N)
    */
   void LinkedList::ReverseRecursive() {
-    // Напишите здесь свой код ...
+    reverse_recursive_helper(front_, nullptr);
+    Node* node = front_;
+    front_ = back_;
+    back_ = node;
   }
 
   // вспомогательный метод для реализации рекурсии
   void LinkedList::reverse_recursive_helper(Node*& curr, Node* prev) {
-    // Напишите здесь свой код ...
+    if (curr == nullptr) return;
+    Node* next = curr->next;
+    curr->next = prev;
+
+    reverse_recursive_helper(next, curr);
   }
 
 }  // namespace assignment
